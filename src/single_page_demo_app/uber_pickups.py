3eb887a1e1e5fab1e1447d21cb918765d94664c5
Dazbo <darren_lester@epam.com>
2024-11-24 17:09:29 +0000

    Built single page uber pickups demo

--- src/single_page_demo_app/uber_pickups.py
+++ src/single_page_demo_app/uber_pickups.py
@@ -10,6 +10,8 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache_data
 def load_data(nrows):
+    # Fetch the Uber dataset for pickups and drop-offs in New York City
+    # It's quite big
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
@@ -17,6 +19,7 @@ def load_data(nrows):
     return data
 
 # Create a text element and let the reader know the data is loading.
+st.subheader("Data Load")
 data_load_state = st.text('Loading data...')
 
 # Load 10,000 rows of data into the dataframe.
@@ -25,7 +28,21 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
-st.subheader('Raw data')
-st.write(data)
+st.subheader('Raw Data')
+with st.expander("Expand raw data", expanded=False):
+    st.write(data)
 
 st.subheader('Number of pickups by hour')
+# Use NumPy to generate a histogram that breaks down pickup times binned by hour
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('Map of all pickups')
+st.map(data)
+
+hour_to_filter = 17
+per_hour_pickups_header = st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+per_hour_pickups_header.subheader(f'Map of all pickups at {hour_to_filter}:00')
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.map(filtered_data)
